47d9732cf19faef10ac05d2d74f81be7a010d7cc
Marcos Aurélio dos Santos <31566851+Marcosausantos@users.noreply.github.com>
2025-04-24 01:19:15 +0000

    Alteração nivel Intermediario

--- atividade_01_fundamentos.c
+++ atividade_01_fundamentos.c
@@ -6,6 +6,9 @@ int main() {
     char estado_1, estado_2, codigo_1[4], codigo_2[4], cidade_1[50], cidade_2[50];
     int populacao_1, populacao_2, pontosturisticos_1, pontosturisticos_2;
     float area_1, area_2, pib_1, pib_2;
+    
+    float densidadePopulacional_1, densidadePopulacional_2, pibPercapto_1, pibPercapto_2;
+
 
     // Carta 1
     printf("\nDigite os dados da Carta 1:\n");
@@ -52,6 +55,12 @@ int main() {
     sprintf(codigo_1, "%c01", estado_1);
     sprintf(codigo_2, "%c02", estado_2);
 
+    densidadePopulacional_1 = (float) populacao_1 / area_1;
+    densidadePopulacional_2 = (float) populacao_2 / area_2;
+
+    pibPercapto_1 = (float) pib_1 / populacao_1;
+    pibPercapto_2 = (float) pib_2 / populacao_2;
+
     printf("\nCarta 01:\n");
     printf("Estado: %c\n", estado_1);
     printf("Código: %s\n", codigo_1);
@@ -60,6 +69,8 @@ int main() {
     printf("Área: %.2f km²\n", area_1);
     printf("PIB: %.2f bilhões de reais\n", pib_1);
     printf("Número de Pontos Turísticos: %d\n", pontosturisticos_1);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidadePopulacional_1);
+    printf("PIB per Capita %.2f reais\n", pibPercapto_1);
 
     printf("\nCarta 02:\n");
     printf("Estado: %c\n", estado_2);
@@ -69,6 +80,8 @@ int main() {
     printf("Área: %.2f km²\n", area_2);
     printf("PIB: %.2f bilhões de reais\n", pib_2);
     printf("Número de Pontos Turísticos: %d\n", pontosturisticos_2);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidadePopulacional_2);
+    printf("PIB per Capita %.2f reais\n", pibPercapto_2);
 
     return 0;
 }
