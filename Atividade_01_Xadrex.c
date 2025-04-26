972954ab1dee43750b14c5d850ba67d81bc3cd78
Marcos Aurélio dos Santos <31566851+Marcosausantos@users.noreply.github.com>
2025-04-26 01:31:13 +0000

    Atividade_02_Xadrez

---
+++ Atividade_01_Xadrex.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void moverTorre() {
+    printf("\nMovimento da Torre (5 casas para a direita):\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Direita\n");
+    }
+}
+
+void moverBispo() {
+    printf("\nMovimento do Bispo (5 casas na diagonal cima-direita):\n");
+    int i = 0;
+    while (i < 5) {
+        printf("Cima, Direita\n");
+        i++;
+    }
+}
+
+void moverRainha() {
+    printf("\nMovimento da Rainha (8 casas para a esquerda):\n");
+    int i = 0;
+    do {
+        printf("Esquerda\n");
+        i++;
+    } while (i < 8);
+}
+
+int main() {
+    // Torre: for (5 casas para a direita)
+    moverTorre();
+    
+    // Bispo: while (5 casas na diagonal cima-direita)
+    moverBispo();
+    
+    // Rainha: do-while (8 casas para a esquerda)
+    moverRainha();
+    
+    return 0;
+}
